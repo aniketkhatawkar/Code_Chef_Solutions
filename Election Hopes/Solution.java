9adb6771bc888eb6634e90a22fbfc30c344da295
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-06-06 14:17:51 +0530

    Add question Election Hopes With Optimal Solution

---
+++ Election Hopes/Solution.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		// take votes of chef and chefu
+		int x = input.nextInt();
+		int y = input.nextInt();
+		
+		//check chef is having twice votes or not
+		if(test>=(n*2)) {
+			System.out.println("YES");
+		}
+		else {
+			System.out.println("NO");
+		}
+	}
+}
