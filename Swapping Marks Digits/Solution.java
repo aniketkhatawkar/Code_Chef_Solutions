d499e202179ca174d5888c7cd81b0da1b42f6e23
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-06-20 22:07:12 +0530

    Add the solutions of Swapping Marks Digits and Television Channels

---
+++ Swapping Marks Digits/Solution.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    int a = input.nextInt();
+		    int b = input.nextInt();
+		    
+		    int reva=(a%10)*10 + (a/10);
+		    int revb=(b%10)*10 + (b/10);
+		    
+
+		    // check any way that alice marks is grater then bob
+		    if(reva>revb || reva>b || a>b || a>revb) {
+		        System.out.println("YES");
+		    }
+		    else {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
