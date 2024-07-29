2ee3821f78fb122594019b7216d71148d5facb66
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-07-29 19:14:32 +0530

    Add Question Airline Restrictions With optimal solution

---
+++ 1000 - 1200 1 Beginner Level/Airline Restrictions/Solution.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    int a = input.nextInt();
+		    int b = input.nextInt();
+		    int c = input.nextInt();
+		    int d = input.nextInt();
+		    int e = input.nextInt();
+		    
+		    if(((a+b)<=d && c<=e) || ((b+c)<=d && a<=e) || ((a+c)<=d && b<=e)) {
+		        System.out.println("YES");
+		    }
+		    else {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
