704793b16edf34f8d16d2edd7c30d298449fe2ed
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-08-24 16:10:40 +0530

    Add Question Odd Pairs with Optimal solution

---
+++ 1000 - 1200 1 Beginner Level/Odd Pairs/Solution.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    long n = input.nextInt();
+		    
+		    System.out.println(n*n/2);
+		}
+	}
+}
