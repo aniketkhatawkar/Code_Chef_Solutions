a47ae77600ed4afd1f20707742b2e2ee39227744
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-10-18 16:55:16 +0530

    Add Question Long Queue with Optimal solution

---
+++ 0 - 1000 Beginner Level/Long Queue/Solution.java
@@ -0,0 +1,42 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    int n = input.nextInt();
+		    
+		    ArrayList<Integer> al = new ArrayList<>();
+		    
+		    for(int i=0; i<n-1; i++){
+		        al.add(input.nextInt());
+		    }
+		    
+		    int x=input.nextInt();
+		    boolean flag=true;
+		    
+		    while(flag && !al.isEmpty()){
+		        if(al.get(al.size()-1)<=(x/2)){
+		            al.remove(al.size()-1);
+		        }
+		        else {
+		            flag=false;
+		        }
+		    }
+		    
+		    System.out.println(al.size()+1);
+		}
+	}
+}
