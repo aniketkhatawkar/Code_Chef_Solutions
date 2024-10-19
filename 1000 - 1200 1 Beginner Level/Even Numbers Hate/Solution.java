2415db92f7d8b194d1b6a0d84a1367dabbaf39d7
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-10-19 22:08:21 +0530

    Add Question Even Numbers Hate with Optimal solution

---
+++ 1000 - 1200 1 Beginner Level/Even Numbers Hate/Solution.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    int n = input.nextInt();
+		    int counto=0, counte=0;
+		    
+		    for(int i=0; i<n; i++){
+		        int temp=input.nextInt();
+		        if(temp%2!=0){
+		            counto++;
+		        }
+		        else {
+		            counte++;
+		        }
+		    }
+		    
+		    if(counto==0){
+		        System.out.println(0);
+		    }else{
+		        System.out.println(((counto+1)/2)+counte);
+		    }
+		}
+	}
+}
