389e61647cd74c9e17b9957a058a92e468dc9f70
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-10-28 22:47:53 +0530

    Add Question IPL and RCB with Optimal solution

---
+++ 1000 - 1200 1 Beginner Level/IPL and RCB/Solution.java
@@ -0,0 +1,29 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    int x = input.nextInt();
+		    int y = input.nextInt();
+		    
+		    if(x<=y){
+                System.out.println(0);
+            }else{
+                System.out.println(x-y);
+            }
+		}
+	}
+}
