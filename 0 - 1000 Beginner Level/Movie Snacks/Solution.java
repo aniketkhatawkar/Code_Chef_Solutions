a85c20b536f0e94b67bf0a3ca8b2bbeddaa2aa98
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-11-14 18:31:24 +0530

    Add Question Hidden Numbers And Movie Snacks with Optimal solution

---
+++ 0 - 1000 Beginner Level/Movie Snacks/Solution.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		    
+	    int x = input.nextInt();
+	    int y = input.nextInt();
+	    int z = input.nextInt();
+	    
+	    System.out.println(Math.min((2*x)+(3*y), (2*z)+y));
+	}
+}
