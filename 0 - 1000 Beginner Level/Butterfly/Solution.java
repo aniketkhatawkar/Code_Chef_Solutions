127c720449935720761e50bdfb2f41873f5f6da6
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-11-26 22:47:12 +0530

    Add Question Butterfly with Optimal solution

---
+++ 0 - 1000 Beginner Level/Butterfly/Solution.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    int R = input.nextInt();
+            int G = input.nextInt();
+            int B = input.nextInt();
+
+            int maxButterflies = Math.max(R, Math.max(G, B));
+            int sumOfOtherColors = R + G + B - maxButterflies;
+
+            if (maxButterflies > sumOfOtherColors) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+		}
+	}
+}
