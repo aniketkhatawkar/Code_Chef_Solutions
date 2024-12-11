3179d2629dadd3f372674292f962e23d31822e90
Aniket Khatavkar <aniketkhatawkar2001@gmail.com>
2024-12-11 18:36:56 +0530

    Add Question Avoid Squares Please with Optimal solution

---
+++ 0 - 1000 Beginner Level/Avoid Squares Please/Solution.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		
+		int test = input.nextInt();
+		
+		for(int t=0; t<test; t++) {
+		    
+		    int n = input.nextInt();
+		    
+		    for(int i = 1; i <= n; i++) {
+	            System.out.print(i+" ");
+	        }
+	        
+		    System.out.println();
+		}
+	}
+}
